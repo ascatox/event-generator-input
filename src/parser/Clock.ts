04e36b6572348e90a1ff98a897488ffeba2a31b5
clod16 <c.barberini94@gmail.com>
2018-06-06 12:00:28 +0200

    Read .txt line-by-line, extract Item

--- src/parser/Clock.ts
+++ src/parser/Clock.ts
@@ -1,7 +1,24 @@
 const scheduler = require("node-schedule");
+import {FileParser} from "./FileParser";
+
+
 class Clock {
 
+    constructor(){
 
+    }
+   
+    private static clock(){
+
+    var event = scheduler.scheduleJob("5 * * * * *", function() {
+        console.log('This runs every 5 second');
+        this.FileParse.parseData();
+        
+
+
+    });
+
+    }
 
 
 }
--- src/parser/FileParser.ts
+++ src/parser/FileParser.ts
@@ -1,9 +1,25 @@
+
 class FileParser {
 
-    constructor(props) {
+    constructor() {
     }
 
-    public parseData(){
+    public parseData() {
+
+        var LineByLineReader = require('../line-by-line.js'),
+            lr = new LineByLineReader('big_file.txt');
+
+        lr.on('error', function (err) {
+            // 'err' contains error object
+        });
+
+        lr.on('line', function (line) {
+            // 'line' contains the current line without the trailing newline character.
+        });
+
+        lr.on('end', function () {
+            // All lines are read, file is closed now.
+        });
     }
 
 }
