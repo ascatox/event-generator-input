f4800f0fb4572524568bd73360f0ab469dcb71e6
clod16 <c.barberini94@gmail.com>
2018-06-06 12:08:46 +0200

    First commit

--- src/parser/Clock.ts
+++ src/parser/Clock.ts
@@ -1,22 +1,36 @@
 const scheduler = require("node-schedule");
-import {FileParser} from "./FileParser";
+//import { eventGeneratorInput } from "../EventGeneratorInput"
+import { ConveyorItem } from "../ledger/model/ConveyorItem";
+import { ConveyorItemType } from "../ledger/model/ConveyorItemType";
 
 
 class Clock {
 
-    constructor(){
+    constructor() {
 
     }
-   
-    private static clock(){
 
-    var event = scheduler.scheduleJob("5 * * * * *", function() {
-        console.log('This runs every 5 second');
-        this.FileParse.parseData();
-        
+    private static clock() {
 
+        var event = scheduler.scheduleJob("5 * * * * *", function () {
+            console.log('EventGeneratorInput start...');
 
-    });
+            let lineRead = this.FileParse.parseData();
+            console.log('Item In on conveyor' +lineRead);
+            let item: ConveyorItem;
+            let itemType: ConveyorItemType = new ConveyorItemType(lineRead[0], lineRead[3]);
+
+            item: { id: lineRead[0] };
+            item: { typeObject: null  };
+            item: { type: this.itemType.description};
+            item: { state: null };
+            item: { bay: null };
+
+            console.log('Query chaincode...');
+            this.eventGeneratorInput.storeConveyorItem(item);
+            console.log('Query done');
+
+        });
 
     }
 
