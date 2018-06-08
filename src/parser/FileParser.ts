3315ace9b2387f2aee879706ae07393a4e54c6c1
clod16 <c.barberini94@gmail.com>
2018-06-08 15:16:53 +0200

    EventGenIN DONE

--- src/parser/FileParser.ts
+++ src/parser/FileParser.ts
@@ -1,44 +1,52 @@
 import { ConveyorItem } from "../model/ConveyorItem";
 import { ConveyorItemType } from "../model/ConveyorItemType";
 import { eventGeneratorInput } from "../EventGeneratorInput";
+import * as log from "../Logger";
+
 class FileParser {
-    private item: ConveyorItem;
-    private itemType: ConveyorItemType;
-    private lines: any;
-    private index: number;
+  private item: ConveyorItem;
+  private itemType: ConveyorItemType;
+  private lines: any;
+  private index: number;
 
-    constructor() {
-        this.index = 0;
-        this.lines = require('fs').readFileSync('../../data/logger_input.txt', 'utf-8').split('\n');
-    }
+  constructor() {
+    this.index = 0;
+    this.lines = require("fs")
+      .readFileSync("logger_input.txt", "utf-8")
+      .split("\n");
+  }
 
-    public parseData() {
-        console.log('Line read: ' + this.lines[this.index]);
-        const arrayRead = this.lines[this.index].split(';');
-        let timestamp = arrayRead[2];
-        setTimeout(() => {
-            const arrayItem = this.lines[this.index].split(';');
-            console.log("ItemIN : " + arrayItem);
-            this.item = {
-                id: arrayItem[0],
-                typeObject: null,
-                type: arrayItem[3],
-                state: null,
-                bay: null
-            };
-            this.itemType = {
-                id: arrayItem[0],
-                description: arrayItem[3]
-            };
-            console.log(this.item);
-            console.log(this.itemType);
-            console.log("Query chaincode...");
-            eventGeneratorInput.storeConveyorItem(this.item);
-            console.log("Query done at " + new Date());
-            this.index++;
-            this.parseData();
-        }, +timestamp);
+  public parseData() {
+    try {
+      log.logger.debug("Line read: " + this.lines[this.index]);
+      const arrayRead = this.lines[this.index].split(";");
+      let timestamp = arrayRead[2];
+      setTimeout(() => {
+        const arrayItem = this.lines[this.index].split(";");
+        log.logger.debug("ItemIN : " + arrayItem);
+        this.item = {
+          id: arrayItem[0],
+          typeObject: null,
+          type: arrayItem[3],
+          state: null,
+          bay: null
+        };
+        this.itemType = {
+          id: arrayItem[0],
+          description: arrayItem[3]
+        };
+        log.logger.debug(this.item);
+        log.logger.debug(this.itemType);
+        log.logger.debug("Query chaincode...");
+        eventGeneratorInput.storeConveyorItem(this.item);
+        log.logger.debug("Query done at " + new Date());
+        this.index++;
+        this.parseData();
+      }, +timestamp);
+    } catch (e) {
+        log.logger.error(e);
     }
+  }
 }
 
 export { FileParser };
