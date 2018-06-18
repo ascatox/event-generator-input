40843c9b8991e6b4541a97ee84eef003a9630f47
clod16 <c.barberini94@gmail.com>
2018-06-18 16:34:05 +0200

    commit

--- src/parser/FileParser.ts
+++ src/parser/FileParser.ts
@@ -20,60 +20,60 @@ class FileParser {
   }
 
   public parseData() {
-    if (this.lines == null || this.lines == undefined || this.lines == '') {
+    if (this.lines == null || this.lines == undefined || this.lines == "") {
       log.logger.debug(" Error Input: line null or empty!!!");
     } else {
-      try {
-        if (this.lines != null || this.lines != undefined) {
-          log.logger.debug("Line read: " + this.lines[this.index]);
+      if (this.lines != null || this.lines != undefined) {
+        log.logger.debug("Line read: " + this.lines[this.index]);
 
-          const arrayRead = this.lines[this.index].split(";");
-          let timestamp = arrayRead[2];
-          setTimeout(() => {
-            const arrayItem = this.lines[this.index].split(";");
-            log.logger.debug(chalk.yellow("ItemIN : " + arrayItem));
-            this.item = {
-              id: arrayItem[0],
-              typeObject: "ITEM",
-              type: this.itemType,
-              state: null,
-              conveyorBay: null
-            };
-            this.itemType = {
-              id: arrayItem[3],
-              description: "null"
-            };
-            if (arrayItem[3] == "869990965260") {
-              this.itemType.description = "oven";
-            }
-            if (arrayItem[3] == "869990965261") {
-              this.itemType.description = "fridge";
-            }
-            if (arrayItem[3] == "869990965262") {
-              this.itemType.description = "washingmachine";
-            }
-            if (arrayItem[3] == "869990965263") {
-              this.itemType.description = "dishwasher";
-            }
-            if (arrayItem[3] == "869990965264") {
-              this.itemType.description = "dryer";
-            }
-            log.logger.debug(this.item);
-            log.logger.debug(this.itemType);
-            log.logger.debug("Query chaincode...");
-
+        const arrayRead = this.lines[this.index].split(";");
+        let timestamp = arrayRead[2];
+        setTimeout(() => {
+          const arrayItem = this.lines[this.index].split(";");
+          log.logger.debug(chalk.yellow("ItemIN : " + arrayItem));
+          this.item = {
+            id: arrayItem[0],
+            typeObject: "ITEM",
+            type: this.itemType,
+            state: null,
+            conveyorBay: null
+          };
+          this.itemType = {
+            id: arrayItem[3],
+            description: "null"
+          };
+          if (arrayItem[3] == "869990965260") {
+            this.itemType.description = "oven";
+          }
+          if (arrayItem[3] == "869990965261") {
+            this.itemType.description = "fridge";
+          }
+          if (arrayItem[3] == "869990965262") {
+            this.itemType.description = "washingmachine";
+          }
+          if (arrayItem[3] == "869990965263") {
+            this.itemType.description = "dishwasher";
+          }
+          if (arrayItem[3] == "869990965264") {
+            this.itemType.description = "dryer";
+          }
+          log.logger.debug(this.item);
+          log.logger.debug(this.itemType);
+          log.logger.debug("Query chaincode...");
+          try {
             eventGeneratorInput.storeConveyorItem(this.item);
             log.logger.info(
               chalk.red(
                 "Query done at " + moment().format("MMMM Do YYYY, h:mm:ss a")
               )
             );
-            this.index++;
-            this.parseData();
-          }, +timestamp);
-        }
-      } catch (e) {
-        log.logger.error(e);
+          } catch (e) {
+            log.logger.error(e);
+          }
+
+          this.index++;
+          this.parseData();
+        }, +timestamp);
       }
     }
   }
